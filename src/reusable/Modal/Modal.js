57c48ade21deb266ac56d5666c2cdadf1f99e052
Eduard Rastoropov <eduard@rastoropov@gmail.com>
2020-02-03 11:11:00 +0200

    Removed unused import

--- src/reusable/Modal/Modal.js
+++ src/reusable/Modal/Modal.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import Backdrop from '../Backdrop/Backdrop';
 import IconButton from '../IconButton/IconButton';
@@ -12,21 +11,21 @@ class Modal extends React.Component {
 		return this.props.visible !== nextProps.visible;
 	}
 
-    render() {
-    	const { children, hideModal, visible } = this.props;
+	render() {
+		const { children, hideModal, visible } = this.props;
 
-        return(
-            <div>
-	       		<Backdrop hide={hideModal} modalVisible={visible} />
-		       	<ModalDiv visible={visible}>
+		return (
+			<div>
+				<Backdrop hide={hideModal} modalVisible={visible} />
+				<ModalDiv visible={visible}>
 					<IconButton onClick={hideModal} type="cross" />
-		       		<div style={{ position: 'relative' }}>
-			        	{children}
-		        	</div>
-		        </ModalDiv>
-	        </div>
-        );
-    }
+					<div style={{ position: 'relative' }}>
+						{children}
+					</div>
+				</ModalDiv>
+			</div>
+		);
+	}
 }
 
 export default Modal;
